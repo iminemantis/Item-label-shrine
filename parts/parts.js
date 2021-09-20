c9b96a8cce6aa85377453c160ac119d6aff7ab40
iminemantis <90356983+iminemantis@users.noreply.github.com>
2021-09-20 12:43:23 +0100

    finishing up the header change
    finished having the header change when the width goes below 600 px, this might change in future. there are some things i wish could be better but this is it for now

--- parts/parts.js
+++ parts/parts.js
@@ -6,81 +6,99 @@ class Header extends HTMLElement {
         this.innerHTML = `
 
         <div id="header">
+        <a href="/Item-label-shrine/"><i class="fas fa-home"></i></a>
 
-            <a href="/Item-label-shrine/"><i class="fas fa-home"></i></a>
-
-            <div class="links">
-                
-                <div class="drop">
-                    <a href="product pages/peepy.html">peepys</a>
-                    <div class="drop-cont">
-                        <a href="product pages/peepy/cowpy.html">cowpy</a>
-                        <a href="product pages/peepy/sheepy.html">sheepy</a>
-                        <a href="product pages/peepy/fuzzpy.html">fuzzpy</a>
-                        <a href="product pages/peepy/celebrity blue.html">celebrity blue</a>
-                        <a href="product pages/peepy/cornpy.html">corndogpy</a>
-                        <a href="product pages/peepy/lemonpy.html">lemonpy</a>
-                        <a href="product pages/peepy/mistakepy.html">mistakepy</a>
-                        <a href="product pages/peepy/pink.html">pinkpy</a>
-                        <a href="product pages/peepy/mushroompy.html">mushroompy</a>
-                        <a href="product pages/peepy/silverpy.html">silverpy</a>
-                        <a href="product pages/peepy/goldenpy.html">goldpy</a>
-                        <a href="product pages/peepy/emeraldpy.html">emeraldpy</a>
-                    </div>
+        <div class="links pc">
+            <div class="drop">
+                <a href="/Item-label-shrine/product pages/peepy.html">peepys</a>
+                <div class="drop-cont">
+                    <a href="#">cowpy</a>
+                    <a href="#">sheepy</a>
+                    <a href="#">fuzzpy</a>
+                    <a href="/Item-label-shrine/product pages/peepy/celebrity blue.html">celebrity blue</a>
+                    <a href="#">corndogpy</a>
+                    <a href="#">lemonpy</a>
+                    <a href="#">mistakepy</a>
+                    <a href="#">pinkpy</a>
+                    <a href="#">mushroompy</a>
+                    <a href="#">silverpy</a>
+                    <a href="#">goldpy</a>
+                    <a href="#">emeraldpy</a>
                 </div>
-
-                <div class="drop">
-                    <a href="#">sucklets</a>
-                    <div class="drop-cont">
-                        <a href="#">sucklet</a>
-                        <a href="#">super sucklet</a>
-                        <a href="#">dark sucklet</a>
-                        <a href="#">poision sucklet</a>
-                        <a href="#">moss sucklet</a>
-                    </div>
-                </div>
-
-                <div class="drop">
-                    <a href="#">oui ouis</a>
-                    <div class="drop-cont">
-                        <a href="#">cow</a>
-                        <a href="#">stinker</a>
-                        <a href="#">lemon</a>
-                        <a href="#">sunny day</a>
-                        <a href="#">gold</a>
-                    </div>
-                </div>
-
-                <div class="drop">
-                    <a href="#">dinkle/moley</a>
-                    <div class="drop-cont">
-                        <a href="#">dinkle</a>
-                        <a href="#">soft dinkle</a>
-                        <a href="#">moley</a>
-                        <a href="#">purple moley</a>
-                    </div>
-                </div>
-
-                <div class="drop">
-                    <a href="#">other softies</a>
-                    <div class="drop-cont">
-                        <a href="#">bad guy</a>
-                        <a href="#">doughnuts</a>
-                        <a href="#">fishcar</a>
-                        <a href="#">fishcopter</a>
-                        <a href="#">brungus</a>
-                    </div>
-                </div>
-
             </div>
-            <div id="theme-tog">
-                <label class="switch">
-                    <input type="checkbox" onclick="toggleTheme()" id="b01" checked="true">
-                    <span class="slider"></span>
-                </label>
+
+            <div class="drop">
+                <a href="#">sucklets</a>
+                <div class="drop-cont">
+                    <a href="#">sucklet</a>
+                    <a href="#">super sucklet</a>
+                    <a href="#">dark sucklet</a>
+                    <a href="#">poision sucklet</a>
+                    <a href="#">moss sucklet</a>
+                </div>
+            </div>
+
+            <div class="drop">
+                <a href="#">oui ouis</a>
+                <div class="drop-cont">
+                    <a href="#">cow</a>
+                    <a href="#">stinker</a>
+                    <a href="#">lemon</a>
+                    <a href="#">sunny day</a>
+                    <a href="#">gold</a>
+                </div>
+            </div>
+
+            <div class="drop">
+                <a href="#">dinkle/moley</a>
+                <div class="drop-cont">
+                    <a href="#">dinkle</a>
+                    <a href="#">soft dinkle</a>
+                    <a href="#">moley</a>
+                    <a href="#">purple moley</a>
+                </div>
+            </div>
+
+            <div class="drop">
+                <a href="#">other softies</a>
+                <div class="drop-cont">
+                    <a href="#">bad guy</a>
+                    <a href="#">doughnuts</a>
+                    <a href="#">fishcar</a>
+                    <a href="#">fishcopter</a>
+                    <a href="#">brungus</a>
+                </div>
             </div>
 
         </div>
+
+        <div class="links mobile">
+            <div class="ma">
+                <label>
+                    <button class="accord" onclick="accord()"></button>
+                    <span>&#x2630</span>
+                </label>
+            </div>
+            <div id="mbl">
+                <a href="/Item-label-shrine/product pages/peepy.html">peepy</a>
+                <a href="#">sucklets</a>
+                <a href="#">oui oui</a>
+                <a href="#">dinkle/moley</a>
+                <a href="#">other softies</a>
+            </div>
+            
+
+
+        </div>
+
+        <div id="theme-tog">
+            <label class="switch">
+                <input type="checkbox" onclick="toggleTheme()" id="b01" checked="true">
+                <span class="slider"></span>
+            </label>
+        </div>
+
+    </div>
         
         `
     }
