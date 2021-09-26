dd613049256fd6041664b2c46c2e2fab15e5aa1e
iminemantis <90356983+iminemantis@users.noreply.github.com>
2021-09-26 18:15:43 +0100

    animation on mobile accordion
    added animation for when the accordion page selector drops down on mobile

--- script.js
+++ script.js
@@ -62,15 +62,18 @@ window.addEventListener("load",checkStyle)
 
 
 //mobile suport
+let open = true;
 function accord(){
-    if (document.getElementById("mbl").style.display=="flex"){
+    if (open == false){
         document.getElementById("mbl").style.paddingTop="0";
-        document.getElementById("mbl").style.display="none";
-        document.getElementById("page").style.marginTop="5vh";
-    }else{
-        document.getElementById("mbl").style.paddingTop="4vh";
         document.getElementById("mbl").style.display="flex";
-        document.getElementById("page").style.marginTop="16vh";
+        document.getElementById("page").style.marginTop="5vh";
+        open = true;
+    }else{
+        document.getElementById("mbl").style.paddingTop="9vh";
+        document.getElementById("mbl").style.display="flex";
+        document.getElementById("page").style.marginTop="15vh";
+        open = false;
     }
 }
 
